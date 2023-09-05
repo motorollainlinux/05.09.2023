213ec4041043da06110fa58c8e2b56199824dd7c
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-09-05 13:52:01 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+
+void EnterResult(int* Arr) {
+    cout << "Result: ";
+    for(int i = 0; i < 100; i++) {
+        cout << Arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+    void Math(int num1, int num2, int* result) {
+    int res = num1 + num2;
+    std::string Str = "";
+    Str = res;
+    for(int i = 0; i < Str.length(); i++) {
+        result[i] = (int)Str[i];
+    }
+    EnterResult(result);
+    res = num1 - num2;
+    Str = "";
+    Str = res;
+    for(int i = 0; i < Str.length(); i++) {
+        result[i] = (int)Str[i];
+    }
+    EnterResult(result);
+    res = num1 * num2;
+    Str = "";
+    Str = res;
+    for(int i = 0; i < Str.length(); i++) {
+        result[i] = (int)Str[i];
+    }
+    EnterResult(result);
+}
+void Nums(int* Arr1, int* Arr2, int* result) {
+    int num1 = 0;
+    int num2 = 0;
+    std::string Str = "";
+    for(int i = 0; Arr1[i] != 0; i++) {
+        Str += Arr1[i];
+    }
+    Math(num1, num2, result);
+}
+//     num1 = std::stoi(Str);
+//     Str = "";
+//     for(int i = 0; Arr2[i] != 0; i++) {
+//         Str += Arr2[i];
+//     }
+//     num2 = std::stoi(Str);
+// }
+
+void UserEnter(int* Arr) {
+   int CountNum = 0;
+   int UNum = 0;
+   bool Correct = false;
+   do {
+       cout << "Enter Count of array cells(1 - 100)\n";
+       cin >> CountNum;
+       if (CountNum > 0 && CountNum <= 100) {
+           Correct = true;
+       } else {
+       cout << "Is incorrect!\n";
+       }
+   }while(!Correct);
+   Correct = false;
+   for(int i = 0; i < CountNum; i++) {
+       do{
+           cout << i+1 << "|Enter number(0 - 9): ";
+           cin >> UNum;
+           cout << "\n";
+           if (UNum >= 0 && UNum <= 9) {
+               Correct = true;
+               Arr[i] = UNum;
+           }
+       }while(!Correct);
+       Correct = false;
+   }
+   for (int i = CountNum; i < 100; i++) {
+       Arr[i] = 0;
+   }
+}
+
+
+int main() {
+    int Arr1[100];
+    int Arr2[100];
+    int ArrResult[101];
+    UserEnter(Arr1);
+    UserEnter(Arr2);
+    Nums(Arr1, Arr2, ArrResult);
+}
+
